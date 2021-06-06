42675a3f13af5e21e9c47d461dd9eb9c913a93a9
skboy <364971779@qq.com>
2021-06-07 02:37:15 +0800

    config->configuration

---
+++ src/main/java/com/skboy/base/configuration/Constant.java
@@ -0,0 +1,11 @@
+package com.skboy.base.configuration;
+
+/**
+ * 描述 常量
+ *
+ * @author skboy
+ * @date 3/6/2021下午6:45
+ */
+public  class Constant {
+
+}
---
+++ src/main/java/com/skboy/base/configuration/XssFilter.java
@@ -0,0 +1,34 @@
+package com.skboy.base.configuration;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @author tangguangwei
+ */
+@WebFilter(urlPatterns = "/*")
+public class XssFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request= (HttpServletRequest) servletRequest;
+        XssHttpServletRequestWrapper wrapper=new XssHttpServletRequestWrapper(request);
+        filterChain.doFilter(wrapper,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
